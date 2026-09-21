c636d9ecff1968d0f5506ae45b4dea0c03ca4572
André Milanez <andremilanezdev@gmail.com>
2026-09-21 20:17:17 -0300

    Adicionado Curso
    Curso adicionado ao projeto

--- src/main/java/com/example/codecamp/controllers/CursoController.java
+++ src/main/java/com/example/codecamp/controllers/CursoController.java
@@ -1,15 +1,15 @@
 package com.example.codecamp.controllers;
 
-
+import com.example.codecamp.DTO.*;
 import com.example.codecamp.entities.Curso;
+import com.example.codecamp.entities.Usuario;
 import com.example.codecamp.repository.CursoRepository;
+import com.example.codecamp.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -17,28 +17,83 @@ import java.util.List;
 public class CursoController {
 
     @Autowired
+
     private CursoRepository cursoRepository;
 
     //Lista todos os cursos
     @GetMapping
-    public List<Curso> ConsultaCursos(){
+    public List<Curso> ConsultaCuros() {
         return cursoRepository.findAll();
     }
 
-    //Busca Curso por ID
+
     @GetMapping("/{id}")
     public ResponseEntity<Curso> ConsultaCursoPorID(@PathVariable Long id){
         var curso = cursoRepository.findById(id).orElse(null);
         if (curso == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(curso);
+        return  ResponseEntity.ok(curso);
+    }
+    //Cadastro curso
+    @PostMapping
+    public ResponseEntity<CursoResponse>CadastrarCurso(@RequestBody Curso cursoRequest){
+        Curso curso = new Curso();
+        curso.setNome(cursoRequest.getNome());
+        curso.setCargahoraria(cursoRequest.getCargahoraria());
+        curso.setDataCadastro(LocalDateTime.now());
+        curso.setStatus("A");
+        //Salvando no banco
+        cursoRepository.save(curso);
+        return ResponseEntity.ok(new CursoResponse(curso.getId(),
+                "Curso Cadastrado com sucesso!"));
     }
 
-    //Consulta por empresa
-    @GetMapping("/empresa/{empresa}")
-    public Curso ConsultaCursoPorEmpresa(@PathVariable Long empresaId){
-        Curso cursocontrutor = new Curso()
+    @PutMapping("/{id}")
+    public ResponseEntity<CursoResponse> AtualizarCurso(@PathVariable Long id, @RequestBody CursoRequest cursoRequest) {
+        //Consuta no banco
+        Curso curso = cursoRepository.findById(id).orElse(null);
+
+        if (curso !=null){
+            curso.setNome(curso.getNome());
+            curso.setCargahoraria(cursoRequest.getCargahoraria());
+            curso.setDataAtualizacao(LocalDateTime.now());
+            cursoRepository.save(curso);
+            return ResponseEntity.ok(new CursoResponse(curso.getId(),
+                    "Curso Atualizado com sucesso!"));
+        }
+        return ResponseEntity.notFound().build();
     }
 
-}//Fim da classe principal
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<CursoResponse> AtualizarStatusCurso(@PathVariable Long id, @RequestBody AtualizaStatusCursoRequest cursoRequest) {
+
+        //Consuta no banco
+        Curso curso = cursoRepository.findById(id).orElse(null);
+
+        if (curso !=null){
+            curso.setStatus(curso.getStatus());
+            cursoRepository.save(curso);
+            return ResponseEntity.ok(new CursoResponse(curso.getId(),
+                    "Status Curso Atualizado com sucesso!"));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CursoResponse> AtualizarStatus(@PathVariable Long id){
+        //Consuta no banco
+        Curso curso = cursoRepository.findById(id).orElse(null);
+        //Deletar do banco
+        //usuarioRepository.delete(curso);
+
+        if (curso !=null){
+            curso.setStatus("D");
+            //salva no banco
+            cursoRepository.save(curso);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
